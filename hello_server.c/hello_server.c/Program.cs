2b7ce74e9eb414992356327da6e5c39f41d1378c
kimsui <k1msu4.com@gmail.com>
2016-02-18 21:37:35 +0900

    0_hello_server.c

---
+++ hello_server.c/hello_server.c/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_server.c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
